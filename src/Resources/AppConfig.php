6ee9603f36b064ac6173edd74a71be92fec5ce34
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2019-03-11 14:38:04 -0300

    Atualizado config database

--- src/Resources/AppConfig.php
+++ src/Resources/AppConfig.php
@@ -3,9 +3,9 @@
 return [
     'database' => [
         'driver'    => 'mysql',
-        'host'      => 'localhost',
-        'user'      => 'root',
-        'pass'      => '',
-        'dbname'    => 'dbPandora'
+        'host'      => 'mysql942.umbler.com',
+        'user'      => 'dbpandora',
+        'pass'      => 'p4nd0r4.2019',
+        'dbname'    => 'dbpandora'
     ]
 ];
