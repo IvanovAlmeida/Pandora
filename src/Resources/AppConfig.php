01c97645fe88f48b1a5d33c1d1280cf19472d963
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2019-03-12 14:19:23 -0300

    Ajustes VIEW

--- src/Resources/AppConfig.php
+++ src/Resources/AppConfig.php
@@ -2,7 +2,7 @@
 return [
     'database' => [
         'driver'    => 'mysql',
-        'host'      => 'mysql942.umbler.com',
+        'host'      => 'mysql942.umbler.com:41890',
         'user'      => 'dbpandora',
         'pass'      => 'p4nd0r4.2019',
         'dbname'    => 'dbpandora'
