14440c732754c7254a17ef01ab7bf26f6ab57071
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2019-03-12 14:16:24 -0300

    Add files via upload

---
+++ src/Resources/AppConfig.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'database' => [
+        'driver'    => 'mysql',
+        'host'      => 'mysql942.umbler.com',
+        'user'      => 'dbpandora',
+        'pass'      => 'p4nd0r4.2019',
+        'dbname'    => 'dbpandora'
+    ]
+];
