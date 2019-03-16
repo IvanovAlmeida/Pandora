8d97804588b6fdf98a6f978ac882f8565b49140a
Ivanov de Almeida <ivanovdealmeida@gmail.com>
2019-03-15 23:09:03 -0300

    Cadastro Cliente

---
+++ src/Model/Entity/Contact.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Model\Entity;
+
+/**
+ * Class Contact
+ * @package App\Model\Entity
+ */
+class Contact extends Entity
+{
+    public $telephone;
+    public $email;
+    public $address;
+    public $city;
+    public $state;
+    public $country;
+    public $zipCode;
+}
---
+++ src/View/Clients/index.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @var \App\Resources\View $this
+ * @var \App\Model\Entity\Client[] $clients
+ */
+?>
+<div class="col-sm-12">
+    <a href="/clientes/cadastrar" class="btn btn-success btn-sm float-right">
+        <i class="fa fa-plus"></i>
+    </a>
+    <h3>Clientes</h3>
+    <hr>
+</div>
+<div class="col-sm-12">
+    <table class="table tableItens">
+        <tbody>
+        <tr>
+            <th>#</th>
+            <th>Nome</th>
+            <th>CPF / CNPJ</th>
+            <th>Telefone</th>
+            <th>E-mail</th>
+            <th class="text-center">
+                Ações
+            </th>
+        </tr>
+        </tbody>
+        <tbody>
+        <?php foreach($clients as $client): ?>
+            <tr>
+                <td><?= $client->id ?></td>
+                <td><?= $client->name ?></td>
+                <td><?= (strlen($client->cpf) > 0) ? $client->cpf : $client->cnpj ?></td>
+                <td><?= $client->telephone ?></td>
+                <td><?= $client->email ?></td>
+                <td class="text-center">
+                    <a class="btn btn-sm btn-warning" href="/clientes/editar/<?= $client->id ?>">
+                        <i class="fa fa-edit"></i>
+                    </a>
+                    <button class="btn btn-sm btn-danger btnApagar text-dark" data-id="<?= $client->id ?>">
+                        <i class="fa fa-trash"></i>
+                    </button>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+</div>
